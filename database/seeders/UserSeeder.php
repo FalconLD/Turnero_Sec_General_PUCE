760e1468eb3d34d5db0215bfb980f9eff3e7a3ab
Cristofer <sebass_lopez@hotmail.es>
2025-09-25 15:00:09 -0500

    Agregar Seeders con usuarios admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,18 +10,21 @@ use App\Models\User;
  
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        User::create([
-            'name'=>'Darling Arroyo',
-            'email'=>'darroyo606@puce.edu.ec',
-            'password'=> bcrypt('12345678')
-        ]);
+        $users = [
+            ['name' => 'Darling Arroyo', 'email' => 'darroyo606@puce.edu.ec', 'password' => '12345678'],
+            ['name' => 'Cristian Freire', 'email' => 'cafreirel@puce.edu.ec', 'password' => '4321'],
+            ['name' => 'Cristhofer Lopez', 'email' => 'clopez559@puce.edu.ec', 'password' => '7410'],
+        ];
+    
+        foreach ($users as $user) {
+            User::create([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => bcrypt($user['password']),
+            ]);
+        }
     }
 }
  

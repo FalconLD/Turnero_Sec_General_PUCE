b0253ea4623f31811b87a7e44cc6a00f0d0feaa2
Cristian <cristianfreire1@gmail.com>
2025-09-25 15:38:29 -0500

    Cambios de texto Loguin v1

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,13 +15,21 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
+    public function run(): void
+{
+    $users = [
+        ['name' => 'Darling Arroyo', 'email' => 'darroyo606@puce.edu.ec', 'password' => '12345678'],
+        ['name' => 'Cristian Freire', 'email' => 'cafreirel@puce.edu.ec', 'password' => '4321'],
+        ['name' => 'Cristhofer Lopez', 'email' => 'clopez559@puce.edu.ec', 'password' => '7410'],
+    ];
+
+    foreach ($users as $user) {
         User::create([
-            'name'=>'Darling Arroyo',
-            'email'=>'darroyo606@puce.edu.ec',
-            'password'=> bcrypt('12345678')
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'password' => bcrypt($user['password']),
         ]);
     }
+}
 }
  

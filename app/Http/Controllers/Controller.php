46777dfba27daec9fd3ea11e31e013b095d98f7f
Cristofer <sebass_lopez@hotmail.es>
2025-09-25 14:36:32 -0500

    Cambio controlador

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,10 +9,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-<<<<<<< HEAD
-    //hola de nuevo
-
-    //Holis Dar XD
-=======
->>>>>>> develop
 }

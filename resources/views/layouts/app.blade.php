644b3aa6864e096bf596038f970552ec8e4214ce
Cristian <cristianfreire1@gmail.com>
2025-10-24 15:16:19 -0500

    prueba

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
-                                </div>
+                                </div>                               
                             </li>
                         @endguest
                     </ul>
